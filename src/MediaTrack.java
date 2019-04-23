72006a9d2159937ff492f2d2227aff3557e34690
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-04-24 04:08:14 +0530

    Update MediaTrack.java

--- src/MediaTrack.java
+++ src/MediaTrack.java
@@ -1,31 +1,31 @@
-//import javafx.application.Platform;
-//
-//import javax.print.attribute.standard.Media;
-//import java.util.Timer;
-//import java.util.concurrent.ScheduledExecutorService;
-//
-//abstract class MediaTrack {
-//    //ScheduledExecutorService uiScheduler;
-//    double length;
-//    Timer timer;
-//    Platform pt;
-//
-//    Player player;
-//
-//
-//
-//    public MediaTrack(double length, String fileUrl, ScheduledExecutorService uiScheduler) {
-//        this.length = length;
-//        this.timer = uiScheduler;
-//        Media media = new Media(fileUrl);
-//        Platform.runLater(() -> {
-//            player = new MediaPlayer(media);
-//            player.setOnError(() ->
-//                    logger.error("Failed to play media "+fileUrl, player.getError())
-//            );
-//            player.setOnEndOfMedia(this::stop
-//            );
-//        });
-//    }
-//
-//}
+import javafx.application.Platform;
+
+import javax.print.attribute.standard.Media;
+import java.util.Timer;
+import java.util.concurrent.ScheduledExecutorService;
+
+abstract class MediaTrack {
+   //ScheduledExecutorService uiScheduler;
+   double length;
+   Timer timer;
+   Platform pt;
+
+   Player player;
+
+
+
+   public MediaTrack(double length, String fileUrl, ScheduledExecutorService uiScheduler) {
+       this.length = length;
+       this.timer = uiScheduler;
+       Media media = new Media(fileUrl);
+       Platform.runLater(() -> {
+           player = new MediaPlayer(media);
+           player.setOnError(() ->
+                   logger.error("Failed to play media "+fileUrl, player.getError())
+           );
+           player.setOnEndOfMedia(this::stop
+           );
+       });
+   }
+
+}
