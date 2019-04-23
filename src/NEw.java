756dfda2de7e002322e8e3dd0294f592529512eb
Ramstein <46052567+RamsteinWR@users.noreply.github.com>
2019-04-24 04:07:06 +0530

    Update NEw.java

--- src/NEw.java
+++ src/NEw.java
@@ -1,24 +1,24 @@
-//public class NEw {
-//    public static void  main(String args[]){
-//        int[][] a={{1,2,3,4,5,6},{7,8,9,1,2,3},{4,5,6,7,8,9},{1,2,3,4,5,6},{7,8,9,1,2,3},{4,5,6,7,8,9,}};
-//        int sum[][] = new int[6][6];
-//        int i1=0,i2=0,i3=0,i4=2,i5=3,i6=3,i7=3;
-//        int j1=0,j2=1,j3=2,j4=2,j5=0,j6=1,j7=2;
-//        for(int r=0; r<4; r++){
-//            for(int c=0; c<3; c++){
-//                sum[r][c] = a[i1][j1]+ a[i2][j2]+ a[i3][j3] + a[i4][j4] +a[i5][j5]+a[i6][j6]+ a[i7][j7];
-//                j1++;j2++;j3++;j4++;j5++;j6++;j7++;
-//            }
-//            i1++;i2++;i3++;i4++;i5++;i6++;i7++;
-//        }
-//        int max = sum[0][0];
-//        for(int i=0;i<5; i++){
-//            for (int j = 0; j < 20; j++) {
-//                if(sum[i][j] > max){
-//                    max = sum[i][j];
-//                }
-//            }
-//        }
-//        System.out.println(max);
-//    }
-//}
+public class NEw {
+   public static void  main(String args[]){
+       int[][] a={{1,2,3,4,5,6},{7,8,9,1,2,3},{4,5,6,7,8,9},{1,2,3,4,5,6},{7,8,9,1,2,3},{4,5,6,7,8,9,}};
+       int sum[][] = new int[6][6];
+       int i1=0,i2=0,i3=0,i4=2,i5=3,i6=3,i7=3;
+       int j1=0,j2=1,j3=2,j4=2,j5=0,j6=1,j7=2;
+       for(int r=0; r<4; r++){
+           for(int c=0; c<3; c++){
+               sum[r][c] = a[i1][j1]+ a[i2][j2]+ a[i3][j3] + a[i4][j4] +a[i5][j5]+a[i6][j6]+ a[i7][j7];
+               j1++;j2++;j3++;j4++;j5++;j6++;j7++;
+           }
+           i1++;i2++;i3++;i4++;i5++;i6++;i7++;
+       }
+       int max = sum[0][0];
+       for(int i=0;i<5; i++){
+           for (int j = 0; j < 20; j++) {
+               if(sum[i][j] > max){
+                   max = sum[i][j];
+               }
+           }
+       }
+       System.out.println(max);
+   }
+}
